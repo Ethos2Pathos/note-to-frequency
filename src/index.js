bc4090612bce550f563d11182d6352caffd583d5
duivvv <geoffrey.dhuyvetters@gmail.com>
2016-12-31 00:43:39 +0100

    :sparkles: added no octave error

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ export default (note: string, {base = 440, maxOctave = 8}: options = {}): number
 
   const {letter, octave, signature} = validateNote(note, {flatToSharp: true, maxOctave});
 
+  if (octave === undefined) throw new Error(`${note} is not a valid note, needs an octave`);
+
   const pos: number = getPosition(letter, octave, signature);
   const basePos: number = getPosition(`A`, 4);
   const diff: number = pos - basePos;
